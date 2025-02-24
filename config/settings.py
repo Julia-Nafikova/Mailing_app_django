a3b19c712446441b39be5e9cac0bd050316e3b69
Nafikova_Julia <nafikova91@yandex.ru>
2025-02-24 13:59:27 +0300

    add home page

--- config/settings.py
+++ config/settings.py
@@ -144,8 +144,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 
-LOGIN_REDIRECT_URL = 'mailing:mailing_list'
-LOGOUT_REDIRECT_URL = 'mailing:mailing_list'
+LOGIN_REDIRECT_URL = 'mailing:main_page'
+LOGOUT_REDIRECT_URL = 'mailing:home'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
